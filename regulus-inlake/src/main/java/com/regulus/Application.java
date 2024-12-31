a610a206602bf9b7b4238e975b238d8cef0db30e
叶清影 <vernonsong@163.com>
2024-12-31 22:51:24 +0800

    ci:

---
+++ regulus-inlake/src/main/java/com/regulus/Application.java
@@ -0,0 +1,10 @@
+package com.regulus;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
